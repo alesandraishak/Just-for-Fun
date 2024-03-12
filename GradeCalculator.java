c553e6e477a71b2e1503ecef816e9cfc2a3c45f3
alesandraishak <114614520+alesandraishak@users.noreply.github.com>
2024-03-12 15:09:04 -0400

    Update GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -5,10 +5,10 @@ public class GradeCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Welcome user and explain functionality
-        System.out.println("Welcome to Alesandra's Grade Calculator!\nTo see instructions, please type 'help' or enter any character to get started.");
+        System.out.println("Welcome to Alesandra's Grade Calculator!\nTo see instructions, please type 'help' or type any character and hit enter to get started.");
         String input = scanner.nextLine();
         if (input.equals("help")){
-            System.out.println("\nBegin by entering your grade on an assessment, followed by it's respective weight.\nRepeat this until you have entered all entities and type 'done' to indicate this.");
+            System.out.println("\nBegin by entering your grade (%) on an assessment, followed by its respective weight (%).\nRepeat this until you have entered all entities and type 'done' to indicate this.");
             System.out.println("\nFrom here you will have the option to calculate the remaining grade you must recieve, to acheive your target grade.\n");
         }
 
@@ -48,37 +48,20 @@ public class GradeCalculator {
                 System.out.print("Please enter your target grade: ");
                 float target = Float.parseFloat(scanner.nextLine());
                 
-                // convert total weight and final (remaining) weighting to decfimal values for the calculations`                                                                                                                                                        
                 float decTotalWeight = (totalWeight)/100;
                 float decFinalWeight = (100 - totalWeight)/100;
 
                 float required = (target - (decTotalWeight * current))/(decFinalWeight);
-                if (required >= 0 && required <= 100){
-                    System.out.println("To finish with a " + target + " you must achieve a grade of " + required + " on your final exam.");
-                }
-                // return if the user would have to get a negative mark for the remainder of their grade
-                else if (required < 0){
-                    System.out.println("It is impossible for your grade to drop to a(n) " + target);
-                }
-                // return if the user would have to get a mark that is greater than 100% for the remainder of their grader2
-                else {
-                    System.out.println("Sorry, it is impossible for you to finish with a(n) " + target);
-                }
-                
+                System.out.println("To finish with a " + target + " you must achieve a grade of " + required + " on your final exam.");
             }
         
         }
-        else if (totalWeight == 0){
+        else {
             System.out.println("Sorry, unable to calculate final grade. No grades entered.");
-        }
-        else if (totalWeight > 100){
-            System.out.println("Oops, your grade makeup is greater than 100%. Please try again.");
-        }
-        else{
-            System.out.println("You have completed all assessments for this course. Congratulations!");
         }
 
         scanner.close();
     }
 }
+
 
